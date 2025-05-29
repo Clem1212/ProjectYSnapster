a8b057a7d73f5b9f346e9c6bf75795f02ca541d0
Clem1212 <0puimLord2900@gmail.com>
2025-05-28 21:22:56 -0400

    Add files via upload

--- frontend/chat_app.php
+++ frontend/chat_app.php
@@ -171,9 +171,13 @@ if ($chat_with && isset($chat_users[$chat_with])) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Private JSON Chat</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snapster</title>
+    <link rel="icon" type="image/x-icon" href="/image/logo-removebg-preview.png">
+
      <link rel="stylesheet" href="/frontend/style.css">
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
